33b43d9b58c16469a6ba75eec6f785f72812fb9d
eucristineycosta@gmail.com <eucristineycosta@gmail.com>
2026-07-31 15:58:08 -0300

    Versão 1

---
+++ main.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+nota1 = float(input('Digite a nota 1: '))
+nota2 = float(input('Digite a nota 2: '))
+def calcular_media(nota1,nota2):
+    return (nota1+nota2)/2
+media = calcular_media(nota1,nota2)
+print(F'Sua média é {media}.')
